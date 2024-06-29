035322c65dbfd15b4c7408efa1b25cb28a3e21fa
Sjoshi24 <135809947+Sjoshi24@users.noreply.github.com>
2024-06-30 01:54:15 +0530

    commit: should return 0 for an empty string Jtest implemented

---
+++ __tests__/stringCalculator.test.ts
@@ -0,0 +1,8 @@
+import { add } from '../src/stringCalculator';
+
+describe('String Calculator', () => {
+  test('should return 0 for an empty string', () => {
+    expect(add('')).toBe(0);
+  });
+});
+
